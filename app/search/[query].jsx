191cba75d1b7474ac5cbc661fdd308548f5d5672
longnt <nice231096@gmail.com>
2024-08-26 18:08:59 +0700

    search input

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,10 +1,13 @@
+import { useLocalSearchParams } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
+  const {query} = useLocalSearchParams();
+
   return (
     <SafeAreaView className="px-4 my-6 bg-primary h-full">
-      <Text className="text-2xl text-white font-psemibold">Search</Text>
+      <Text className="text-2xl text-white font-psemibold">{query}</Text>
     </SafeAreaView>
   );
 };
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from "react-native";
 
 import { icons } from "../constants";
+import { router, usePathname } from "expo-router";
 
 const SearchInput = ({
   value,
@@ -10,17 +11,33 @@ const SearchInput = ({
   otherStyles,
   ...props
 }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState('')
+
   return (
     <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center space-x-4">
         <TextInput
             className="text-base mt-0.5 text-white flex-1 font-pregular"
-            value={value}
+            value={query}
             placeholder='Search for a video topic'
-            placeholderTextColor="#7B7B8B"
-            onChangeText={handleChangeText}
+            placeholderTextColor="#CDCDE0"
+            onChangeText={(e) => setQuery(e)}
         />
 
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            if (!query) {
+              return Alert.alert('Missing query', 'Please input something to search results');
+            }
+
+            if (pathname.startsWith('/search')) {
+              router.setParams({query})
+            }
+            else {
+              router.push(`/search/${query}`)
+            }
+          }}
+        >
             <Image 
                 source={icons.search}
                 className='w-5 h-5'
