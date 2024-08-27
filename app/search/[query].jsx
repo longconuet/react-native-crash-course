f5e41fe0ce50edb151c8c7bcb084ea36df94e98b
longnt <nice231096@gmail.com>
2024-08-27 10:18:45 +0700

    search page

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,48 @@
-import { useLocalSearchParams } from "expo-router";
-import { Text } from "react-native";
+import { FlatList, Text, View, Image, RefreshControl, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState, useEffect } from "react";
+
+import { images } from "../../constants";
+import { SearchInput, Trending, EmptyState, VideoCard } from "../../components";
+import { searchPosts } from "../../lib/appwrite";
+import useAppwrite from '../../lib/useAppwrite';
+import { useLocalSearchParams } from "expo-router";
 
 const Search = () => {
   const {query} = useLocalSearchParams();
 
+  const [refreshing, setRefreshing] = useState(false)
+  const {data: posts, refetch} = useAppwrite(() => searchPosts(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
   return (
-    <SafeAreaView className="px-4 my-6 bg-primary h-full">
-      <Text className="text-2xl text-white font-psemibold">{query}</Text>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList 
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({item}) => (
+          <VideoCard video={item}/>
+        )}
+        ListHeaderComponent={() => (
+          <View className='my-6 px-4'>
+            <Text className='font-pmedium text-sm text-gray-100'>Search Results</Text>
+            <Text className='text-2xl font-psemibold text-white'>{query}</Text>
+
+            <View className='mt-6 mb-8'>
+              <SearchInput initialQuery={query} />
+            </View>     
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState 
+            title='No Videos Found'
+            subtitle='No videos found for this search query' 
+          />
+        )}
+      />
     </SafeAreaView>
   );
 };
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -9,10 +9,10 @@ const SearchInput = ({
   placeholder,
   handleChangeText,
   otherStyles,
-  ...props
+  initialQuery
 }) => {
   const pathname = usePathname();
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState(initialQuery || '')
 
   return (
     <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center space-x-4">
@@ -38,11 +38,11 @@ const SearchInput = ({
             }
           }}
         >
-            <Image 
-                source={icons.search}
-                className='w-5 h-5'
-                resizeMode="contain"
-            />
+          <Image 
+              source={icons.search}
+              className='w-5 h-5'
+              resizeMode="contain"
+          />
         </TouchableOpacity>
     </View>
   );
